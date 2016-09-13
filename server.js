777bd6865fa384e26b70b19ea4a3c6b5290a573b
Richard Sage <richardsage@VideoConferenceMacMini.local>
2016-09-13 12:00:57 -0300

    server changes to work with heroku and localhost

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ var express = require('express');
 
 // Create app
 var app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.use(function(req, res, next) {
-    if(req.headers['x-forwarded-proto'] === 'http') {
-        next();
-    }
-    else {
-        res.redirect('http://' + req.hostname + req.url);
-    }
-});
+if(PORT === undefined) {
+    app.use(express.static('public'));
 
-app.use(express.static('public'));
+    app.listen(3000, function() {
+        console.log('Express server on port ' + PORT);
+    });
+}
+else {
+    app.use(function(req, res, next) {
+        if(req.headers['x-forwarded-proto'] === 'http') {
+            next();
+        }
+        else {
+            res.redirect('http://' + req.hostname + req.url);
+        }
+    });
 
-app.listen(PORT, function() {
-    console.log('Express server on port ' + PORT);
-});
+    app.use(express.static('public'));
+
+    app.listen(PORT, function() {
+        console.log('Express server on port ' + PORT);
+    });
+}
