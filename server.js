f354e1d79a1bbc04b24e4c59592b626d3654db82
Richard Sage <richardsage@VideoConferenceMacMini.local>
2016-09-13 12:09:58 -0300

    new fix for server

--- server.js
+++ server.js
@@ -4,26 +4,22 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT;
 
-if(PORT === undefined) {
-    app.use(express.static('public'));
+app.use(express.static('public'));
 
-    app.listen(3000, function() {
-        console.log('Express server on port 3000');
-    });
-}
-else {
-    app.use(function(req, res, next) {
-        if(req.headers['x-forwarded-proto'] === 'http') {
-            next();
-        }
-        else {
-            res.redirect('http://' + req.hostname + req.url);
-        }
-    });
+app.listen(3000, function() {
+    console.log('Express server on port 3000');
+});
+app.use(function(req, res, next) {
+    if(req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+    else {
+        next();
+    }
+});
 
-    app.use(express.static('public'));
+app.use(express.static('public'));
 
-    app.listen(PORT, function() {
-        console.log('Express server on port ' + PORT);
-    });
-}
+app.listen(PORT, function() {
+    console.log('Express server on port ' + PORT);
+});
