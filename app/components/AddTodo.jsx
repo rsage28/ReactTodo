185cee191743c6ec763f96d5f2907b01c40dc70b
Richard Sage <richardsage@VideoConferenceMacMini.local>
2016-09-13 16:22:56 -0300

    AddTodo tests

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+    onSubmit: function(e) {
+        e.preventDefault();
+        var text = this.refs.todoText.value;
+
+        if(text.length > 0) {
+            this.refs.todoText.value = '';
+            this.props.onAddTodo(text);
+        }
+        else {
+            this.refs.todoText.focus();
+        }        
+    },
+    render: function() {
+        return (
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" ref="todoText" placeholder="Enter a task"/>
+                    <button className="button expanded">Add Todo</button>
+                </form>
+            </div>
+        )
+    }
+});
+
+module.exports = AddTodo;
