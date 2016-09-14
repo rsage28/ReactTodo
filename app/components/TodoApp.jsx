7caef010cffc147af6d1bd160643527b941f1a93
Richard Sage <richardsage@VideoConferenceMacMini.local>
2016-09-14 12:39:46 -0300

    added test and checkboxes for todo items

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,19 +12,23 @@ var TodoApp = React.createClass({
             todos: [
                 {
                     id: uuid(), 
-                    text: 'Walk cat'
+                    text: 'Walk cat',
+                    completed: false
                 },
                 {
                     id: uuid(), 
-                    text: 'Feed human'
+                    text: 'Feed human',
+                    completed: true
                 },
                 {
                     id: uuid(),
-                    text: 'Slap person'
+                    text: 'Slap person',
+                    completed: false
                 },
                 {
                     id: uuid(),
-                    text: 'Get tv'
+                    text: 'Get tv',
+                    completed: true
                 }
             ]
         };
@@ -35,7 +39,8 @@ var TodoApp = React.createClass({
                 ...this.state.todos, 
                 {
                     id: uuid(),
-                    text: text
+                    text: text,
+                    completed: false
                 }
             ]
         });
@@ -46,12 +51,25 @@ var TodoApp = React.createClass({
             searchText: searchText.toLowerCase()
         });
     },
+    handleToggle: function(id) {
+        var updatedTodos = this.state.todos.map((todo) => {
+            if(todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+            
+            return todo;
+        });
+
+        this.setState({
+            todos: updatedTodos
+        });
+    },
     render: function() {
         var {todos} = this.state;
         return (
             <div>
                 <TodoSearch onSearch={this.handleSearch}/>
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onToggle={this.handleToggle}/>
                 <AddTodo onAddTodo={this.handleAddTodo}/>
             </div>
         )
